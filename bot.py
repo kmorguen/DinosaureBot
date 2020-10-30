853b3ab8886747011fa562792f5bc85330363b0b
bb-28 <bb-28@MacBook-Pro-de-bb-28.local>
2020-10-30 22:03:19 +0000

    can do nothing

---
+++ bot.py
@@ -0,0 +1,21 @@
+from PIL import ImageGrab
+import pyautogui
+
+
+class Cordinates():
+    replayBtn = ()
+
+
+def restarGame():
+    pyautogui.click(Cordinates.replayBtn)
+
+def pressSpace():
+    pyautogui.keyDown('space')
+    time.sleep(0.05)
+    print("Jump")
+    pyautogui.keyUp('space')
+
+restarGame()
+time.sleep(1)
+pressSpace()
+
